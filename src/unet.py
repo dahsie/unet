9141cc9f9a440d5c0205e7d26a6b835765cba0df
dahsie <dahsie0653@gmail.com>
2024-08-24 15:26:58 +0200

    adding docstring

--- src/unet.py
+++ src/unet.py
@@ -74,19 +74,17 @@ class UNet(Module):
 		
         
 	def forward(self, x):
-		"""
-		Forward pass of the UNet model.
-
-        Parameters:
-        -----------
-        x : torch.Tensor
-            The input tensor, typically a batch of images with dimensions [batch_size, channels, height, width].
-
-        Returns:
-        --------
-        torch.Tensor
-            The output tensor representing the segmentation map. The dimensions of the output
-            will match `ouput_dim` if `retainDim` is set to True.
+		"""Forward pass of the UNet model
+		
+		Parameters:
+		----------
+		x: torch.Tensor
+            The input tensor, typically a batch images with diemensions [batch_size, channels, height; width].
+		
+		Returns:
+		-------
+		torch.Tensor :
+            The output tensor representing the segmentation map. The dimensions of the output will match 'output_dim' if retain_dim is set to True.
 		"""
 		
 		# Extract features from the encoder
